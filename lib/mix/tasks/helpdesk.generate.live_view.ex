a776666cd8bfe3eb161cc7ce660a54b1dffe1f9f
Srikanth Kyatham <srikanth.1.kyatham@gmail.com>
2024-10-05 13:35:40 +0300

    index.html.heex addition for a resource is working

--- lib/mix/tasks/helpdesk.generate.live_view.ex
+++ lib/mix/tasks/helpdesk.generate.live_view.ex
@@ -1,52 +1,59 @@
 defmodule Mix.Tasks.Helpdesk.Generate.LiveView do
   import Mix.Tasks.Helpdesk.Generate.Util
 
-  def add_ash_table_component(igniter) do
-    webmodule = web_module(igniter)
-    table_component_module = (webmodule <> ".Ash.Table") |> string_to_module_name()
+  def add_live_views(igniter, domain, modules) when is_list(modules) do
+    # live views
+    # inject the proper things - refine
+    # show
+    # index
+    # form
+    modules = [List.first(modules)]
+
+    Enum.reduce(modules, igniter, fn module, igniter ->
+      add_live_view(igniter, domain, module)
+    end)
   end
 
-  def add_live_view(igniter, domain, module) do
+  defp add_live_view(igniter, domain, module) do
     # inject live specific stuff
     # create the path
     # add modules with the code for the respective
     # more deduction
     # create index, show, form
+    web_module = web_module(igniter)
     ash_table_component = table_component_name(igniter)
+
+    Igniter.Code.Module.parse("#{web_module}.live.#{module}live.Index")
+
     # module plural name
     module_plural_name = ""
     id = "#{module_plural_name}_id"
     resource_live_path = module_plural_name
 
-    igniter
-    |> Igniter.Code.Module.create_module(module, """
-    <.container class="py-16">
-      <.live_component
-        id="#{id}"
-        limit={10}
-        offset={0}
-        sort={{"id", :asc}}
-        read_options={[{:tenant, @current_tenant}]}
-        module={#{ash_table_component}}
-        resource={#{module}}
-        resource_live_path={#{resource_live_path}}
-        query={#{module}}
-        api={#{domain}}
-        resource_id={@resource_id}
-        live_action={@live_action}
-        tenant={@current_tenant}
-        url={@url}
-      />
-    </.container>
-    """)
-  end
+    code =
+      """
+        <.container class="py-16">
+        <.live_component
+          id="#{id}"
+          limit={10}
+          offset={0}
+          sort={{"id", :asc}}
+          read_options={[{:tenant, @current_tenant}]}
+          module={#{ash_table_component}}
+          resource={#{module}}
+          resource_live_path={#{resource_live_path}}
+          query={#{module}}
+          api={#{domain}}
+          resource_id={@resource_id}
+          live_action={@live_action}
+          tenant={@current_tenant}
+          url={@url}
+        />
+      </.container>
+      """
 
-  defp add_live_views(igniter, domain, modules) when is_list(modules) do
-    ash_table_component = table_component_name(igniter)
-
-    Enum.reduce(modules, igniter, fn module, igniter ->
-      add_live_view(igniter, domain, module)
-    end)
+    path = get_module_heex_file_path(igniter, module, "index.html.heex")
+    Igniter.create_new_file(igniter, path, code)
   end
 
   def add_live_views_to_router(igniter, modules) do
@@ -55,9 +62,30 @@ defmodule Mix.Tasks.Helpdesk.Generate.LiveView do
   defp add_index_files(igniter, module) do
   end
 
+  defp add_index_html_heex_file(igniter, module) do
+  end
+
+  defp add_index_ex_file(igniter, module) do
+  end
+
   defp add_show_files(igniter, module) do
   end
 
   defp add_form_files(igniter, module) do
+  end
+
+  defp get_module_name(igniter, module) do
+    web_module = web_module(igniter)
+    module_name = Module.split(module) |> List.last()
+    # Module.safe_concat([web_module, Live, module, Index])
+    Igniter.Code.Module.parse("#{web_module}.Live.#{module_name}Live.Index")
+  end
+
+  defp get_module_heex_file_path(igniter, module, file_name) do
+    module_name = get_module_name(igniter, module)
+    dbg()
+    path = Igniter.Project.Module.proper_location(igniter, module_name)
+    dirname = Path.dirname(path)
+    Path.join([dirname, file_name])
   end
 end
