57294f50e0269524c6c0badcc80dabfaf4e45b8f
Srikanth Kyatham <srikanth.1.kyatham@gmail.com>
2024-10-07 16:54:35 +0300

    adding ash json api

---
+++ lib/helpdesk_web/ash_json_api_router.ex
@@ -0,0 +1,5 @@
+defmodule HelpdeskWeb.AshJsonApiRouter do
+  use AshJsonApi.Router,
+    domains: [],
+    open_api: "/open_api"
+end
--- lib/helpdesk_web/router.ex
+++ lib/helpdesk_web/router.ex
@@ -14,6 +14,17 @@ defmodule HelpdeskWeb.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/api/json" do
+    pipe_through [:api]
+
+    forward "/swaggerui",
+            OpenApiSpex.Plug.SwaggerUI,
+            path: "/api/json/open_api",
+            default_model_expand_depth: 4
+
+    forward "/", HelpdeskWeb.AshJsonApiRouter
+  end
+
   scope "/", HelpdeskWeb do
     pipe_through :browser
 
