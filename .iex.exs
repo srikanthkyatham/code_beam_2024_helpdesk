5922c7cd9308f0504fe6bfb8d8305fa198c0f3f7
Srikanth Kyatham <srikanth.1.kyatham@gmail.com>
2024-10-10 18:43:37 +0300

    Fixing documentation of fn in .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -3,9 +3,10 @@ defmodule AshHelpers do
   """
   create_resource usgae
 
-  iex> AshHelpers.create_resource(Org, [name: "org2", slug: "org2"], nil)
+
   """
 
+  #   iex> AshHelpers.create_resource(Org, [name: "org2", slug: "org2"], nil)
   def create_resource(module, params, tenant) do
     module
     |> Ash.Changeset.for_create(:create, params,
