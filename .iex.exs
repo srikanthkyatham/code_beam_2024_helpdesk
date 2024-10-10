0d3c7c654789de17c99943a666b788606b6cc13f
Srikanth Kyatham <srikanth.1.kyatham@gmail.com>
2024-10-10 18:50:29 +0300

    removing document blocks in .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,11 +1,5 @@
 defmodule AshHelpers do
   @moduledoc false
-  """
-  create_resource usgae
-
-
-  """
-
   #   iex> AshHelpers.create_resource(Org, [name: "org2", slug: "org2"], nil)
   def create_resource(module, params, tenant) do
     module
@@ -16,10 +10,6 @@ defmodule AshHelpers do
     )
     |> Ash.create()
   end
-
-  """
-  Api token for a given user
-  """
 
   def create_user_token(user) do
     AshAuthentication.Jwt.token_for_user(user)
